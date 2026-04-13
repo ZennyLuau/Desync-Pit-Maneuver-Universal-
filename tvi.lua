034d3f5fe36a0ed9304f2bd1b82da2c329ceb146
ZennyLuau <zennyofcidk@gmail.com>
2026-04-13 19:37:23 +0700

    Update tvi.lua

--- tvi.lua
+++ tvi.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "ZenithViking | Tactical System",
    LoadingTitle = "Initializing Systems...",
-   LoadingSubtitle = "by Tâm",
+   LoadingSubtitle = "by Nigga",
    ConfigurationSaving = { Enabled = false },
    KeySystem = false,
    Theme = {
